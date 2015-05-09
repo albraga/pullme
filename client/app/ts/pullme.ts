df968452d25b7e28226190fce60d83113ee24fae
Alexandre Braga <alexandre@brahgah.com>
2015-05-09 18:29:27 +0000

    added ts support

---
+++ client/app/ts/pullme.ts
@@ -0,0 +1,22 @@
+module Pullme {
+
+    class User {
+
+	constructor(public id: number, public email: string) {
+	    
+	}
+    }
+
+    class Product {
+
+    }
+
+    class Store {
+
+    }
+
+    class Vicinity {
+
+    }
+    
+}
