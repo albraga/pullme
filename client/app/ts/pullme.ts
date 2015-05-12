bc23e92c67d5ea0ee5d0c84d3ef16f51f2495abe
Alexandre Braga <alexandre@brahgah.com>
2015-05-12 00:59:26 +0000

    pullme module expanded

--- client/app/ts/pullme.ts
+++ client/app/ts/pullme.ts
@@ -2,21 +2,23 @@ module Pullme {
 
     class User {
 
-	constructor(public id: number, public email: string) {
-	    
+	constructor(public id: number, public lat: number, public long: number, public  name: string,  public email: string) {    
 	}
     }
 
     class Product {
-
+	constructor(public id: number, public isDiscount: boolean, public category: string, public price: number) {
+	}
     }
 
     class Store {
-
+	constructor(public id: number, public lat: number, public long: number, public name: string, public address: string, public phone: string) {
+	}
     }
 
     class Vicinity {
-
+	constructor(public id: number, public lat: number, public long: number, public radiusFromUser: number,  public stores: Array<Store>) {
+	}
     }
     
 }
