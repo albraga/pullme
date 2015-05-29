bed8bf2e03b5053bcebd5569e95adc46f2cb1b0f
Alexandre Braga <alexandre@brahgah.com>
2015-05-29 18:56:27 +0000

    slim

--- client/app/js/main.js
+++ client/app/js/main.js
@@ -1,13 +1,9 @@
 $(function() {
 
-var api = restful('http://pullme.pe.hu/rest/index.php/api/widgets');
-
-    var widget = api.one('widgets', 1);
-
-widget.get().then(function(response) {
-    var widgetEntity = response.body();
-    var wid = widgetEntity.data();
-    alert(wid.name);
+    $.getJSON("http://pullme.pe.hu/slim/", function(data) {	
+	alert(data[0]);
+    });
+    alert("nada!");
+    
 });
-
-});
+ 
