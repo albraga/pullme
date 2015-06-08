78e985255fe3bdd9a6f6d0618bf3313ac6d33a88
Alexandre Braga <alexandre@brahgah.com>
2015-06-08 17:36:30 +0000

    settimeout 2000

--- client/app/js/main.js
+++ client/app/js/main.js
@@ -6,29 +6,29 @@ var pullme = (function($) {
     
     var getLocation = function () {
 	if (navigator.geolocation) {
-	    navigator.geolocation.getCurrentPosition(setPosition);
+	    navigator.geolocation.getCurrentPosition(setPositionAndStart);
 	}
     };
 
-    var setPosition = function (position) {
+    var setPositionAndStart = function (position) {
 	setTimeout(function() {
 	    longi = position.coords.longitude;
 	    lat = position.coords.latitude;
 	    startMap();
-	}, 6000);
+	}, 2000);
     };
 
     var initMap = function() {
 	var mapOptions = {
-	    zoom: 14,
+	    zoom: 16,
 	    center: new google.maps.LatLng(lat, longi)
 	};
 	map = new google.maps.Map(document.getElementById('main'), mapOptions);
-    }
+    };
 
     var startMap = function () {
 	google.maps.event.addDomListener(window, 'load', initMap());
-    }
+    };
 
     var app = {
 	initialize: function () {
@@ -39,20 +39,20 @@ var pullme = (function($) {
 	    getLocation();
 	}
     };
-    
-    var controller = {};
-    controller.get = $.getJSON("http://pullme.pe.hu/slim/", function(data) {
-	var user = new model.User(1, lat, longi, data[0], data[1]);
-	alert(user.longi);
-    });
 
     app.initialize();
     
+    var controller = {
+	get: function() {
+	    $.getJSON("http://pullme.pe.hu/slim/", function(data) {
+		var user = new model.User(1, lat, longi, data[0], data[1]);
+		alert('ueh');
+	    });
+	}
+    }
+	
     return controller;
     
-})(jQuery);
-
-$(function() {
-    pullme.get();
-});
+}
+)(jQuery);
 
