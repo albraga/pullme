1ecbb143e8230beb1130aeae343a01158240558a
Alexandre Braga <alexandre@brahgah.com>
2015-06-09 19:54:00 +0000

    haversine.php

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -55,7 +55,7 @@ var pullme = (function($) {
 	    lat = position.coords.latitude;
 	    startMap();
 	    putUserMarker();
-	}, 2000);
+	}, 0);
     };
 
     var putUserMarker = function() {
@@ -91,6 +91,18 @@ var pullme = (function($) {
     };
 
     app.initialize();
+
+    haversine = function() {
+	var radians = Array.prototype.map.call(arguments, function(deg) { return deg/180.0 * Math.PI; });
+	var lat1 = radians[0], lon1 = radians[1], lat2 = radians[2], lon2 = radians[3];
+	var R = 6372.8; // km
+	var dLat = lat2 - lat1;
+	var dLon = lon2 - lon1;
+	var a = Math.sin(dLat / 2) * Math.sin(dLat /2) +
+	    Math.sin(dLon / 2) * Math.sin(dLon /2) * Math.cos(lat1) * Math.cos(lat2);
+	var c = 2 * Math.asin(Math.sqrt(a));
+	return R * c;
+    };
     
     var controller = {
 	get: function() {
@@ -106,3 +118,6 @@ var pullme = (function($) {
 }
 )(jQuery);
 
+$(function() {
+    alert(haversine(-7.083333, -34.833333, -7.080000, -34.83300));
+});
---
+++ server/slim/vendor/slim/slim/haversine.php
@@ -0,0 +1,27 @@
+<?php
+class Haversine {
+    private $latA;
+    private $lonA;
+    private $latB;
+    private $lonB;
+    
+    public function __construct($latA, $lonA, $latB, $lonB) {
+        $this->latA = deg2rad($latA);
+        $this->lonA = deg2rad($lonA);
+        $this->latB = deg2rad($latB);
+        $this->lonB = deg2rad($lonB);
+    }
+    public function get() {
+        $radiusOfEarth = 6371000;// Earth's radius in meters.
+        $diffLatitude = $this->latB - $this->latA;
+        $diffLongitude = $this->lonB - $this->lonA;
+        $a = sin($diffLatitude / 2) * sin($diffLatitude / 2) +
+           cos($this->latA) * cos($this->latB) *
+           sin($diffLongitude / 2) * sin($diffLongitude / 2);
+        $c = 2 * asin(sqrt($a));
+        $distance = $radiusOfEarth * $c;
+        return $distance;
+    }
+}
+$h = new Haversine(7, 34, 7.001, 34.001);
+echo $h->get();
