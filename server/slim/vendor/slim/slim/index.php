ef9206d045f863d11df369ad9cce2bd0f21749cf
Alexandre Braga <alexandre@brahgah.com>
2015-06-25 16:57:08 +0000

    stall 2

--- server/slim/vendor/slim/slim/index.php
+++ server/slim/vendor/slim/slim/index.php
@@ -11,8 +11,8 @@ $app->get('/stores/:maxDistance/:productName/:lat/:lon',
               $st2 = new Store(2, $lat + 0.003, $lon,'carrefour','f','g','h','images/cart2.png');
               $st3 = new Store(3, $lat, $lon + 0.004,'todo ida','j','k','l','images/cart3.png');
               $st4 = new Store(4, $lat, $lon + 0.005,'box 1','j','k','l','images/stall.png');
-              $st5 = new Store(5, $lat, $lon + 0.0052,'box 2','j','k','l','images/stall.png');
-              $st6 = new Store(6, $lat, $lon + 0.0054,'box 3','j','k','l','images/stall.png');
+              $st5 = new Store(5, $lat, $lon + 0.00503,'box 2','j','k','l','images/stall2.png');
+              $st6 = new Store(6, $lat, $lon + 0.00506,'box 3','j','k','l','images/stall3.png');
 
               $withinReach = array();
 
