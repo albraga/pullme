6ab77a4619d55a204fc595f0bd643125a308b94a
Alexandre Braga <alexandre@brahgah.com>
2015-07-23 19:34:24 +0000

    dumb data from database

--- server/index.php
+++ server/index.php
@@ -8,20 +8,16 @@ include('store.php');
 $app = new \Slim\Slim();
 $app->get('/stores/:maxDistance/:productName/:lat/:lon',
           function($maxDistance, $productName, $lat, $lon) use($app) {
-              $storess = getStores();
-              $st1 = new Store(1, $lat, $lon + 0.002,$storess[0]->name,'a','bc','d','images/cart1.png');
-              $st2 = new Store(2, $lat + 0.003, $lon,$lon,'f','g','h','images/cart2.png');
-              $st3 = new Store(3, $lat, $lon + 0.004,'todo ida','j','k','l','images/cart3.png');
-              $st4 = new Store(4, $lat, $lon + 0.005,'box 1','j','k','l','images/stall.png');
-              $st5 = new Store(5, $lat, $lon + 0.00505,'box 2','j','k','l','images/stall2.png');
-              $st6 = new Store(6, $lat, $lon + 0.00510,'box 3','j','k','l','images/stall3.png');
-              $st7 = new Store(7, $lat - 0.00720, $lon - 0.00720,'terceirao','j','k','l','images/terceirao.jpg');
-              
-              $withinReach = array();
+              $storesTemp = getStores();
+              $storess = array();
 
-              $stores = array($st1, $st2, $st3, $st4, $st5, $st6, $st7);
-              
-               foreach($stores as $store) {
+              foreach($storesTemp as $o) {
+  $storess[] = new Store($o->id_store,$o->lat,$o->lon,$o->name,$o->address,$o->phone,$o->products,$o->image);
+              }              
+
+              $withinReach = array();
+       
+               foreach($storess as $store) {
                   if ($store->getDistanceFromUser($lat, $lon) <= $maxDistance) {
                       $withinReach[] = $store->toArray();
                       }
