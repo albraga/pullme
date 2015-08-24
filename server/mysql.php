5fe78ffcb5edb4139080b0113d5daf3818e4b3a2
Alexandre Braga <alexandre@brahgah.com>
2015-08-24 19:16:34 +0000

    haversine via sql

--- server/mysql.php
+++ server/mysql.php
@@ -14,13 +14,15 @@ function getConnection() {
     return $conn;
 }
 
-function getStores($productName) {
+function getStores($productName, $latA, $lonA, $maxDistance) {
     $sql_query =
-"SELECT * FROM store s INNER JOIN store_product sp ON s.store_name = sp.store_name WHERE sp.product_name = :productName AND sp.ispromo = true ";
+"SELECT * FROM store s INNER JOIN store_product sp ON s.store_name = sp.store_name WHERE sp.product_name = :productName AND sp.ispromo = true AND DEGREES(ACOS(COS(RADIANS(:latA)) * COS(RADIANS(s.lat)) *
+             COS(RADIANS(:lonA) - RADIANS(s.lon)) +
+             SIN(RADIANS(:latA)) * SIN(RADIANS(s.lat)))) * 111.045 <= :maxDistance";
     try {
         $dbCon = getConnection();
         $stmt   = $dbCon->prepare($sql_query);
-        $stmt->execute(array(':productName' => $productName));
+        $stmt->execute(array(':productName'=> $productName,':latA'=> $latA,':lonA'=> $lonA,':maxDistance'=> $maxDistance));
         $stores  = $stmt->fetchAll(PDO::FETCH_OBJ);
         $dbCon = null;
     } catch(PDOException $e) {
--- server/service.php
+++ server/service.php
@@ -8,24 +8,16 @@ include('store.php');
 $app = new \Slim\Slim();
 $app->get('/stores/:maxDistance/:productName/:lat/:lon',
           function($maxDistance, $productName, $lat, $lon) use($app) {
-              $storesTemp = getStores($productName);
-              $storess = array();
+              $storesTemp = getStores($productName, $lat, $lon, $maxDistance);
+              $stores = array();
 
               foreach($storesTemp as $o) {
-  $storess[] = new Store($o->store_name,$o->lat,$o->lon,$o->address,$o->phone,$o->image);
+  $stores[] = new Store($o->store_name,$o->lat,$o->lon,$o->address,$o->phone,$o->image);
               }              
 
-              $withinReach = array();
-       
-               foreach($storess as $store) {
-                  if ($store->getDistanceFromUser($lat, $lon) <= $maxDistance) {
-                      $withinReach[] = $store->toArray();
-                      }
-                  }
-            
               $response = $app->response();
               $response->header('Access-Control-Allow-Origin', '*');
-              $response->write(json_encode($withinReach));
+              $response->write(json_encode($stores));
              
 });
 
